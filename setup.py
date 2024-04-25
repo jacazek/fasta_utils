19a3368c712b65d86e343c5b26168d0bc99e4b3f
jake <dukob86@yahoo.com>
2024-04-25 00:04:40 -0400

    feat: install dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup, find_packages
+import os
 
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = f"{lib_folder}/requirements.txt"
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
 setup(
     name='fasta_utils',
     version='0.1.1',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=install_requires
 )
